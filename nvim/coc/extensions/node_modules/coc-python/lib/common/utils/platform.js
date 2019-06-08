8da3109e2c48104412f11ccdee5c2605a57d1922
li-shihao <me@shihao.li>
2019-06-08 19:56:04 +0800

    testing coc

---
+++ nvim/coc/extensions/node_modules/coc-python/lib/common/utils/platform.js
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+var Architecture;
+(function (Architecture) {
+    Architecture[Architecture["Unknown"] = 1] = "Unknown";
+    Architecture[Architecture["x86"] = 2] = "x86";
+    Architecture[Architecture["x64"] = 3] = "x64";
+})(Architecture = exports.Architecture || (exports.Architecture = {}));
+var OSType;
+(function (OSType) {
+    OSType["Unknown"] = "Unknown";
+    OSType["Windows"] = "Windows";
+    OSType["OSX"] = "OSX";
+    OSType["Linux"] = "Linux";
+})(OSType = exports.OSType || (exports.OSType = {}));
+//# sourceMappingURL=platform.js.map

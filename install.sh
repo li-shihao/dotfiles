1c788348f52c539f1eb5fc3883e1c4ae17ee3b97
Li Shihao <me@shihao.li>
2019-06-08 19:11:11 +0800

    added coc and some dasht sources

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ brew install bat
 brew install fzf
 brew install wget
 brew install rg
+brew install aria2
 brew install alder
 brew install --HEAD ccls
 brew install dasht
@@ -24,7 +25,7 @@ curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh >
 chmod +x installer.sh && sh ./installer.sh ~/.cache/dein
 
 # set up marker
-wget https://raw.githubusercontent.com/liangguohuan/fzf-marker/master/fzf-marker.plugin.zsh ~/.local/share/marker/
+aria2c https://raw.githubusercontent.com/liangguohuan/fzf-marker/master/fzf-marker.plugin.zsh ~/.local/share/marker/
 git clone --depth=1 https://github.com/pindexis/marker ~/.marker && ~/.marker/install.py
 
 # set up fzf
@@ -34,6 +35,16 @@ $(brew --prefix)/opt/fzf/install
 source ~/.zshrc
 tmux source ~/.tmux.conf
 ~/.tmux/plugins/tpm/scripts/install_plugins.sh
+
+# set up dasht
+dasht-docsets-install bash
+aria2c http://tokyo.kapeli.com/feeds/zzz/user_contributed/build/IntelASMx86/IntelASMx86.tgz http://tokyo.kapeli.com/feeds/zzz/user_contributed/build/PyTorch/PyTorch.tgz http://tokyo.kapeli.com/feeds/zzz/user_contributed/build/Keras/Keras.tgz http://tokyo.kapeli.com/feeds/zzz/user_contributed/build/pwntools/pwntools.tgz http://tokyo.kapeli.com/feeds/zzz/user_contributed/build/Scikit/scikit-learn.tgz
+
+dasht-docsets-extract PyTorch.tgz
+dasht-docsets-extract IntelASMx86.tgz
+dasht-docsets-extract keras.tgz
+dasht-docsets-extract pwntools.tgz 
+dasht-docsets-extract scikit-learn.tgz 
 
 # set up vim
 curl -sL install-node.now.sh/lts | sh
